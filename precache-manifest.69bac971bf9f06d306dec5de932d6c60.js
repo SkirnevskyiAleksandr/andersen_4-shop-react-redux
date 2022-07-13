6aa8ff0dc0a9104d80644a253d8c0811f6016832
SkirnevskyiAleksandr <sanya-sha2016@ukr.net>
2022-07-13 21:47:45 +0300

    Updates

---
+++ precache-manifest.69bac971bf9f06d306dec5de932d6c60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "087daaf0aee98cb9e606",
+    "url": "/andersen_4-shop-react-redux/static/css/main.11891659.chunk.css"
+  },
+  {
+    "revision": "087daaf0aee98cb9e606",
+    "url": "/andersen_4-shop-react-redux/static/js/main.087daaf0.chunk.js"
+  },
+  {
+    "revision": "b205f6a1709b6ed3aace",
+    "url": "/andersen_4-shop-react-redux/static/js/1.b205f6a1.chunk.js"
+  },
+  {
+    "revision": "9e5649f2aa7d5cd02b52",
+    "url": "/andersen_4-shop-react-redux/static/js/runtime~main.9e5649f2.js"
+  },
+  {
+    "revision": "28cfc1ac35176214080ec2ec641cbd57",
+    "url": "/andersen_4-shop-react-redux/static/media/add_to_basket.28cfc1ac.svg"
+  },
+  {
+    "revision": "bfcc3f686440be6fba7fd60aaaeef99a",
+    "url": "/andersen_4-shop-react-redux/static/media/basket.bfcc3f68.svg"
+  },
+  {
+    "revision": "4d2cd2b2c52eba0a46752fe32e8b70ad",
+    "url": "/andersen_4-shop-react-redux/index.html"
+  }
+];
